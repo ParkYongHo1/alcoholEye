a08e8baace218065c3473b7c24475d293c103192
jhchon <31531119+jhchon@users.noreply.github.com>
2023-11-13 22:37:26 +0900

    회원가입 기능

--- routes/api.ts
+++ routes/api.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import pool from '../server/pool';
 import fs from 'fs';
 import path from 'path';
-const router= express.Router();
+import bcrypt from 'bcrypt';
+const router = express.Router();
 
 router.get('/idCheck', async (req, res, next) => {
   const user = JSON.parse(req.query.data as string);
@@ -40,6 +41,33 @@ router.get('/signUp', async (req, res, next) => {
     next();
   }
   return res.status(500);
+});
+
+router.get('/signIn', async (req, res, next) => {
+  const userData = JSON.parse(req.query.data as string);
+  const userSelectSql = 'select * from users where id = ?';
+  try {
+    const [user] = await pool.query(userSelectSql, [userData.id]);
+    if (Array.isArray(user) && user.length > 0) {
+      const firstUserRow = user[0];
+      if (firstUserRow && 'password' in firstUserRow) {
+        const result = await bcrypt.compare(userData.pw, firstUserRow.password);
+        if (result) {
+          return res.send("OK");
+        } else {
+          return res.send("비밀번호가 일치하지 않음");
+        }
+      } else {
+        return res.send("user 행에 'password'가 없음");
+      }
+    } else {
+      return res.send("가입되지 않은 아이디입니다.");
+    }
+  } catch (error) {
+    console.log(error);
+    next();
+    return res.send("서버오류");
+  }
 });
 
 export default router;
