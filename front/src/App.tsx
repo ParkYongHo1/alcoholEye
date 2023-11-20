4367339d8f9a038981a72e9e674e6e16ed95afc2
jhchon <31531119+jhchon@users.noreply.github.com>
2023-11-20 10:19:24 +0900

    필요 없는 코드 제거

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
 import router from './Router/RoutePath';
 import { RouterProvider } from 'react-router-dom';
-import './css/login.css';
-import './css/main.css';
-import loginLogo from './img/logo.jpg';
-import mainLogo from './img/mainLogo.png';
 
 function App() {
-  const [member, setMember] = useState('');
   return (
     <>
       <RouterProvider router={router} />
